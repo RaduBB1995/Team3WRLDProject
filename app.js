a81c1199b5ae59869d0fcd40dd904f70a8db09c3
JamesFlaherty <JamesFlaherty@computing.dundee.ac.uk>
2018-09-11 17:46:49 +0100

    Implemented indoor navigation and exit button

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const Wrld = require("wrld.js")
 
 const map = Wrld.map("map", "65367fd6a1254b28843e482cbfade28d", {
 
-	center: [40.73061, -73.935242],
+	center: [56.4620, -3],
 	
 	zoom: 16,
 	
-	indoorsEnabled: true
+	indoorsEnabled: true,
+
+
 })
+var indoorControl = new WrldIndoorControl('widget-container', map);	
