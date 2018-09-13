c87ebce7ec39fb65edf55b40822c478049d34791
CraigChicken <CraigChicken@computing.dundee.ac.uk>
2018-09-13 17:17:29 +0100

    Implemented a basic slider for viewing mock time-based data and only appears when the user is viewing an indoor map. Also adjusted starting location of the map to focus on our intended testing location and animated the viewpoint upon entering the indoor map

--- app.js
+++ app.js
@@ -2,12 +2,34 @@ const Wrld = require("wrld.js")
 
 const map = Wrld.map("map", "65367fd6a1254b28843e482cbfade28d", {
 
-	center: [56.4620, -3],
+	center: [56.459900, -2.977970],
 	
-	zoom: 16,
+	zoom: 18,
 	
 	indoorsEnabled: true,
 
 
 })
 var indoorControl = new WrldIndoorControl('widget-container', map);	
+
+function onEnter(event) {
+    console.log("Entered indoor map: " + event.indoorMap.getIndoorMapName());
+	document.getElementById("hidingslider").style.display = "block";
+	setTimeout(function() {
+			map.setView([56.460196, -2.978106], 19.75, {
+			headingDegrees: 169,
+			animate: true,
+			durationSeconds:2
+			});
+	}, 0);
+	setTimeout(function() {
+		map.setCameraTiltDegrees(20);
+	}, 2000);
+}
+
+function onExit(event) {
+    console.log("Exited indoor map");
+	document.getElementById("hidingslider").style.display = "none";
+}
+map.indoors.on("indoormapenter", onEnter);
+map.indoors.on("indoormapexit", onExit);
